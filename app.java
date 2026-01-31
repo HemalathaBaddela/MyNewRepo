7c408c1bc11a0ab6c58cb59b7f664db2418cd94c
HemalathaBaddela <hemabaddela24@gmail.com>
2026-01-31 16:14:38 +0530

    Implement Session class with length functionality
    Added a new Session class with a constructor and length method.

--- app.java
+++ app.java
@@ -6,3 +6,19 @@ class HelloWorld {
         System.out.println("Hello, World!");
     }
 }gg
+
+    
+class Session:
+    def __init__(self, number = 0):
+      self.number = number
+    
+    def __len__(self):
+      return self.number
+
+
+# default length is 0
+session1 = Session()
+print(len(session1))  # Output: 0
+
+session2 = Session(6)
+print(len(session2))  # Output: 6
