8ce4c7fc73a4fa0ee9674d41960bf82f10c44cb6
root <root@ip-172-31-16-182.ec2.internal>
2026-01-31 10:48:00 +0000

    new code

--- app.java
+++ app.java
@@ -22,3 +22,20 @@ print(len(session1))  # Output: 0
 
 session2 = Session(6)
 print(len(session2))  # Output: 6
+
+
+
+class Session:
+    def __init__(self, number = 0):
+      self.number = number
+
+    def __len__(self):
+      return self.number
+
+
+# default length is 0
+session1 = Session()
+print(len(session1))  # Output: 0
+
+session2 = Session(6)
+print(len(session2))  # Output: 6
